a6d1204e9c56ce62adb98ea3ab5f88c4548206a0
Marinus Klasen <marinus@mklasen.nl>
2020-10-17 15:18:22 +0200

    Run composer install after init

--- config/init.sh
+++ config/init.sh
@@ -1,2 +1,4 @@
+echo "Run composer"
+composer install 
 echo "Maybe create database $NAME on $NAME-db"
 docker exec -i ${NAME}-db mysql -uroot -pmysql_password <<< "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
