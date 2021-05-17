b1b4abcf0c7ee9ddfe80a31c72a3cdb568781f32
Marinus Klasen <marinus@mklasen.nl>
2021-05-17 11:07:45 +0000

    Remove old files

--- app/www/content/advanced-cache.php
+++
@@ -1,12 +0,0 @@
-<?php 
-defined( 'ABSPATH' ) || exit;
-define( 'BREEZE_ADVANCED_CACHE', true );
-if ( is_admin() ) { return; }
-if ( ! @file_exists( '/home/501407.cloudwaysapps.com/ucpeqkwjad/public_html/wp-content/plugins/breeze/breeze.php' ) ) { return; }
-$config = '/home/501407.cloudwaysapps.com/ucpeqkwjad/public_html/wp-content/breeze-config/breeze-config.php';
-if ( empty( $config ) || ! @file_exists( $config ) ) { return; }
-$GLOBALS['breeze_config'] = include $config;
-if ( empty( $GLOBALS['breeze_config'] ) || empty( $GLOBALS['breeze_config']['cache_options']['breeze-active'] ) ) { return; }
-if ( @file_exists( '/home/501407.cloudwaysapps.com/ucpeqkwjad/public_html/wp-content/plugins/breeze/inc/cache/execute-cache.php' ) ) {
-	include_once '/home/501407.cloudwaysapps.com/ucpeqkwjad/public_html/wp-content/plugins/breeze/inc/cache/execute-cache.php';
-}
--- app/www/content/breeze-config/breeze-config.php
+++
@@ -1,26 +0,0 @@
-<?php 
-defined( 'ABSPATH' ) || exit;
-return array (
-  'homepage' => 'https://wordpress-501407-1717097.cloudwaysapps.com',
-  'cache_options' => 
-  array (
-    'breeze-active' => '1',
-    'breeze-ttl' => '',
-    'breeze-minify-html' => '0',
-    'breeze-minify-css' => '0',
-    'breeze-minify-js' => '0',
-    'breeze-gzip-compression' => '1',
-    'breeze-desktop-cache' => '1',
-    'breeze-browser-cache' => '1',
-    'breeze-mobile-cache' => '1',
-    'breeze-disable-admin' => '1',
-    'breeze-display-clean' => '1',
-    'breeze-include-inline-js' => '0',
-    'breeze-include-inline-css' => '0',
-  ),
-  'disable_per_adminuser' => '1',
-  'exclude_url' => 
-  array (
-  ),
-); 
-
