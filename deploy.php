864d8eafeaa3cf4d6e41c22a5525c7ce9e38cf17
Marinus Klasen <marinus@mklasen.nl>
2021-05-17 11:04:02 +0000

    Update with latest version

--- deploy.php
+++ deploy.php
@@ -3,24 +3,25 @@ namespace Deployer;
 
 require 'recipe/common.php';
 
-set( 'application', 'wptest' );
-set( 'repository', 'git@github.com:mklasen/timeline.git' );
+
+set( 'application', 'reserveerjewinkel' );
+set( 'repository', 'git@github.com:conference7/reserveerjewinkel.git' );
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set( 'git_tty', true );
+
+// Shared files/dirs between deploys
+set( 'shared_files', array() );
+set( 'shared_dirs', array() );
+set( 'writable_dirs', array() );
+
 set( 'composer_options', 'install --optimize-autoloader' );
 
-// Staging staging
-host( 'staging' )
-	->stage( 'staging' )
-	->set( 'branch', 'master' )
-	->hostname( 'kingfisher' )
-	->set( 'deploy_path', '~/sites/timeline' )
-	->set( 'keep_releases', 3 );
-
-// Production host
 host( 'prod' )
 	->stage( 'prod' )
 	->set( 'branch', 'master' )
-	->hostname( '_hostname_or_ip_' )
-	->set( 'deploy_path', '_path_to_deploy_folder_' )
+	->hostname( 'bijnoabers' )
+	->set( 'deploy_path', '/home/master/sites/reserveerjewinkel/deploy' )
 	->set( 'keep_releases', 5 );
 
 // Symlinks
@@ -30,30 +31,24 @@ task(
 		writeln( ' - Symlink media directories' );
 		run( 'ln -nfs {{deploy_path}}/shared/uploads {{release_path}}/app/www/content/uploads' );
 
-		writeln( ' - Symlink htaccess' );
-		run( 'ln -nfs {{deploy_path}}/shared/.htaccess {{release_path}}/app/www/.htaccess' );
-
 		writeln( ' - Copy wp-config.php' );
 		run( '[ -f {{deploy_path}}/shared/wp-config.php ] && cp {{deploy_path}}/shared/wp-config.php {{release_path}}/app/www/wp-config.php || cp {{release_path}}/app/www/wp-config.php {{deploy_path}}/shared/wp-config.php' );
 
 		writeln( ' - Environment symlink' );
 		run( 'ln -nfs {{deploy_path}}/shared/.env {{release_path}}/.env' );
+		
+		writeln( ' - htaccess symlink' );
+		run( 'ln -nfs {{deploy_path}}/shared/.htaccess {{release_path}}/app/www/.htaccess' );
 
-		writeln( ' - Debug symlink' );
-		run( 'touch {{deploy_path}}/shared/debug.log && ln -nfs {{deploy_path}}/shared/debug.log {{release_path}}/app/www/content/debug.log' );
+		writeln( ' - Archive symlink' );
+		run( 'ln -nfs /var/www/comwifi-medelana/archived-sites {{release_path}}/app/www/archive' );
+
+		writeln( ' - USA symlink' );
+		run( 'ln -nfs /var/www/comwifi-medelana/archived-sites/usa {{release_path}}/app/www/usa' );
 	}
 );
 
-// Restart PHP-FPM
-task(
-	'deploy:restart_php-fpm',
-	function() {
-		writeln( ' - Restart PHP-FPM' );
-		run( '[ -f ~/tools/cloudways-manage-server/restart-php-fpm.php ] && php ~/tools/cloudways-manage-server/restart-php-fpm.php || echo "restart script not found."' );
-	}
-);
-
-// Robots.txt for staging environments
+// Robots.txt - we want this on all environments except for production
 task(
 	'deploy:robots_txt',
 	function () {
@@ -78,20 +73,11 @@ task(
 	}
 );
 
-// Must use plugins.
 task(
-	'deploy:mu-plugins',
-	function () {
-		$stage = null;
-		if ( input()->hasArgument( 'stage' ) ) {
-			$stage = input()->getArgument( 'stage' );
-		}
-		if ( 'prod' === $stage ) {
-			// Don't do this for production
-		} else {
-			writeln( ' - Symlink mu-plugins' );
-			run( 'ln -nfs {{deploy_path}}/shared/mu-plugins {{release_path}}/app/www/content/mu-plugins' );
-		}
+	'deploy:clear_cache',
+	function() {
+		// run( "wp eval \"do_action( 'warpdrive_cache_flush' );\" --path=/var/www/comwifi-medelana/wordpress/current/" );
+		// run( "touch ~/wordpress/php-fpm.service" );
 	}
 );
 
@@ -108,16 +94,14 @@ task(
 		'deploy:writable',
 		'deploy:vendors',
 		'deploy:wp_symlinks',
-		'deploy:mu-plugins',
 		'deploy:robots_txt',
 		'deploy:clear_paths',
 		'deploy:symlink',
-		'deploy:restart_php-fpm',
+		'deploy:clear_cache',
 		'deploy:unlock',
 		'cleanup',
 		'success',
 	)
 );
-
 // [Optional] If deploy fails automatically unlock.
 after( 'deploy:failed', 'deploy:unlock' );
